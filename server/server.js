f9f370034e876e30a4365ec339572bcad7f446e3
Alexandre Angrignon <alex040892@gmail.com>
2024-03-30 15:02:45 +1300

    Register API in Node/Express JS

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ const db = mysql.createConnection({
   database: "signup",
 });
 
+app.post("/register", (req, res) => {
+  const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)";
+  const values = [req.body.name, req.body.email, req.body.password];
+});
+
 app.listen(8081, () => {
   console.log(colors.cyan("Running..."));
 });
